cce307c8bd20c985ca8a648936dab5aec169617c
pengdragon <1466771313@qq.com>
2018-11-30 21:59:16 +0800

    登录模型初步完成

--- app/js/sign_in.js
+++ app/js/sign_in.js
@@ -5,41 +5,58 @@ const sign_in = (function(){
             this.$form_box = document.querySelector(".main .form-box");
             this.$left = this.$form_box.querySelector('.left');
             this.$right = this.$form_box.querySelector('.right');
-            //console.log( this.$left)
+            this.$left_span =  this.$left.lastElementChild;
+            this.$right_span =  this.$right.lastElementChild;
+            console.log(this.$right_span);
             this.$middle = this.$form_box.querySelector('.middle');
             this.$left_mask = this.$middle.firstElementChild;
             this.$right_mask = this.$middle.lastElementChild;
             //console.log(  this.$left_mask)
+            this.$sign = document.querySelector('.sign');
+            this.$form_input = this.$sign.firstElementChild;
+            console.log(this.$form_input);
             this.event();
         },
         event(){
             var _this = this;
-            this.$left.onclick = function(){  
-               
+            this.$left.onclick = e=>{  
+                e.returnValue= false;
+                //点击左边，登录隐藏，扫码手机显示
+                this.$middle.style.display="block"; 
+                this.$sign.style.display = 'none';      
+                //点击左边，右边的span要隐藏；
+                this.$right_span.style.display="none";
+                //左边的显示
+                this.$left_span.style.display="block";
             }
-            this.$middle.onmouseenter = function(){
-                console.log(4)
-                console.log(_this.$left)
-                if(_this.$left){
-                    let newMove = new StartMove(_this.$left_mask); 
-                    newMove.animateTo({left:-80},100,res=>{
+            this.$right.onclick =e=>{
+                e.returnValue= false;
+                //点击右边，登录显示，扫码手机隐藏
+                this.$middle.style.display="none";
+                this.$sign.style.display = "block";
+                //点击右边，左边的span要隐藏；
+                this.$left_span.style.display="none";
+                //右边的显示
+                this.$right_span.style.display="block";
+            }
+            this.$middle.onmouseenter = _=>{
+                if(this.$middle){
+                    let newMove = new StartMove(this.$left_mask); 
+                    newMove.animateTo({left:-80},200,res=>{
+                        this.$right_mask.style.display="block";
                     });
                 }
                
             }
-            this.$middle.onmouseleave = function(){
-                console.log(4)
-                console.log(_this.$left)
-                if(_this.$left){
-                    let newMove = new StartMove(_this.$left_mask); 
-                    newMove.animateTo({left:80},100,res=>{
-                    });
+            this.$middle.onmouseleave = _=>{
+                if(this.$middle){
+                    let newMove = new StartMove(this.$left_mask); 
+                    this.$right_mask.style.display="none";
+                    newMove.animateTo({left:80},200);
                 }
                
             }
-            this.$right.onclick = function(){
-
-            }
+           
             //this.
         }
     }
